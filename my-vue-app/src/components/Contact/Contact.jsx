57ff96e629f9b1c948f4976413b7e4b41ec5db0f
Harshit <mailmehere090@gmail.com>
2024-09-27 17:34:02 +0530

    Contact_Page

--- my-vue-app/src/components/Contact/Contact.jsx
+++ my-vue-app/src/components/Contact/Contact.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Navbar } from '../Navbar/Navbar'
 import './Contact.css'
 export const Contact = () => {
   return (
-    <div className='contact'>
-        <div className="card">
-          
-
-        </div>
-       
+    
+      <div className="contact-page">
+            <h1>Contact Us</h1>
+            <div className="contact-info">
+                <h2>Get in Touch</h2>
+                <p>If you have any questions, feel free to reach out to us.</p>
+                <h3>Address</h3>
+                <p>NIT Delhi Health Care Center<br />Delhi, India</p>
+                <h3>Phone</h3>
+                <p>+91 12345 67890</p>
+                <h3>Email</h3>
+                <p>contact@nitdelhihealthcare.ac.in</p>
+            </div>
     </div>
   )
 }
--- my-vue-app/src/main.jsx
+++ my-vue-app/src/main.jsx
@@ -5,10 +5,10 @@ import { PrimeReactProvider } from 'primereact/api';
 import 'primeflex/primeflex.css';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-// import App from './App.jsx'
+import App from './App.jsx'
 // import Doctor from './components/DoctorPage/Doctor.jsx'
-import './index.css'
-import Login from './components/Auth/Login.jsx'
+// import './index.css'
+// import Login from './components/Auth/Login.jsx'
 // import Test from './test.jsx'
 // import Editor from "./components/Editor/Editor.jsx"
 // import {Footer} from './components/Footer/Footer.jsx'
@@ -16,9 +16,9 @@ import Login from './components/Auth/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <Doctor/> */}
-    <Login/>
+    {/* <Login/> */}
     {/* <Footer/> */}
     {/* <Editor/> */}
     {/* <Appointment/> */}
