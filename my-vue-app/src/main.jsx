b5511913028791e29396c82aff245eb9aa10804d
Harshit <mailmehere090@gmail.com>
2024-09-27 17:16:24 +0530

    dup

---
+++ my-vue-app/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+// import App from './App.jsx'
+// import Doctor from './components/DoctorPage/Doctor.jsx'
+import './index.css'
+import Login from './components/Auth/Login.jsx'
+// import Test from './test.jsx'
+// import Editor from "./components/Editor/Editor.jsx"
+// import {Footer} from './components/Footer/Footer.jsx'
+// import { Appointment } from './components/Appointment/Appointment.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    {/* <Doctor/> */}
+    <Login/>
+    {/* <Footer/> */}
+    {/* <Editor/> */}
+    {/* <Appointment/> */}
+    {/* <Test/> */}
+  </StrictMode>,
+)
