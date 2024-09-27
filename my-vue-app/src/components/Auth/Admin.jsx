3efe067ea3d466e8943e56d3f52ba5d4537a65ec
DeathHauler <bhavayvashisth@gmail.com>
2024-09-27 11:37:31 +0530

    6

--- my-vue-app/src/components/Auth/Admin.jsx
+++ my-vue-app/src/components/Auth/Admin.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import './Doctor.css'
+import React, { useState } from 'react';
+import './Doctor.css';
+import SampleDemo from './Otp';
+
 export const Admin = () => {
+  const [otp, setOtp] = useState(true); // Use boolean for better readability
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission
+    setOtp(false); // Set otp to false to show the next UI
+  };
+
   return (
     <div>
-        <form action="" method='get'>
-            <div className="email">
+      {otp ? ( 
+        <form onSubmit={handleSubmit}> {/* Use onSubmit for the form */}
+          <div className="email">
             <label htmlFor="fmail">Enter the Email:</label>
-            <input type="email"/>
-            </div>
-            <div className="password">
+            <input type="email" id="fmail" required />
+          </div>
+          <div className="password">
             <label htmlFor="fpass">Enter the Password:</label>
-            </div>
-            <input type="password"/>
-            <div className="remember">
-                <input type="checkbox" id='checkbox' />
-                <label htmlFor="checkbox">Remember Me</label>
-            </div>
-            <div className="submit">
-            <input type="submit" value="Submit"/>
-            </div>
-            <div className="forget">
-                <a href="">Forgot Password?</a>
-            </div>
+            <input type="password" id="fpass" required />
+          </div>
+          <div className="remember">
+            <input type="checkbox" id='checkbox' />
+            <label htmlFor="checkbox">Remember Me</label>
+          </div>
+          <div className="submit">
+            <input type="submit" value="Submit" />
+          </div>
+          <div className="forget">
+            <a href="">Forgot Password?</a>
+          </div>
         </form>
+      ) : (
+        <SampleDemo/>
+      )}
     </div>
-  )
-}
+  );
+};
--- my-vue-app/src/main.jsx
+++ my-vue-app/src/main.jsx
@@ -5,20 +5,20 @@ import { PrimeReactProvider } from 'primereact/api';
 import 'primeflex/primeflex.css';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import App from './App.jsx'
-import Doctor from './components/DoctorPage/Doctor.jsx'
+// import App from './App.jsx'
+// import Doctor from './components/DoctorPage/Doctor.jsx'
 import './index.css'
 import Login from './components/Auth/Login.jsx'
 // import Test from './test.jsx'
 // import Editor from "./components/Editor/Editor.jsx"
 // import {Footer} from './components/Footer/Footer.jsx'
-import { Appointment } from './components/Appointment/Appointment.jsx'
+// import { Appointment } from './components/Appointment/Appointment.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Doctor/> */}
-    {/* <Login/> */}
+    <Login/>
     {/* <Footer/> */}
     {/* <Editor/> */}
     {/* <Appointment/> */}
